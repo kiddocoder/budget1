244a1627e25360e0ab8ee49e244511e1712acb22
kiddocoder <ndayikezatresor9@gmail.com>
2024-08-14 13:08:15 +0200

    commit

--- src/componnents/Header.jsx
+++ src/componnents/Header.jsx
@@ -5,7 +5,7 @@ const Header = () =>{
       return (
             <>
             <section className="header flex items-center justify-center text-white w-full h-[250px] bg-cover bg-center">
-                  <div>
+                  <div className="flex flex-col gap-3">
                         <p className="font-light text-xl">Available budget in september : </p>
                         <p className="text-4xl text-center">+1,12548</p>
                         <div className="flex justify-between p-2 bg-emerald-500">
