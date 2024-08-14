35eb90c1233d99df91dc07269e1949e6f333ef2d
kiddocoder <ndayikezatresor9@gmail.com>
2024-08-14 16:56:10 +0200

    update

--- src/App.js
+++ src/App.js
@@ -34,14 +34,14 @@ const App = () => {
   return (
       <>
          <Header total={total}/>
-         <form className="flex gap-5" onSubmit={(event)=>{handleSubmit(event)}}>
-            <select className="p-2" name='type' onChange={(e) =>{handlechange(e)}} required>
+         <form className="flex gap-5 my-5 justify-center items-center" onSubmit={(event)=>{handleSubmit(event)}}>
+            <select className="p-3 w-[100px] rounded-[5px]" name='type' onChange={(e) =>{handlechange(e)}} required>
                <option value="income">+</option>
                <option value="expense">-</option>
             </select>
-            <input className="p-2" name='description' type="text" placeholder='Add description' onChange={(e) =>{handlechange(e)}} required/>
-            <input className="p-2" name='value' type="number"  min={0.00} step={0.1} placeholder='value' onChange={(e) =>{handlechange(e)}} required/>
-            <button type='submit' className="p-2 border-neutral-800">Add</button>
+            <input className="p-3 w-[450px] rounded-[5px]" name='description' type="text" placeholder='Add description' onChange={(e) =>{handlechange(e)}} required/>
+            <input className="p-3 w-[100px] rounded-[5px]" name='value' type="number"  min={0.00} step={0.1} placeholder='value' onChange={(e) =>{handlechange(e)}} required/>
+            <button type='submit' className="p-3 border-neutral-800 bg-green-800 rounded-[5px] text-white text-[18px] cursor-pointer ">Add</button>
          </form>
          <TableResult data={datas}/>
       </>
--- src/componnents/TableResult.jsx
+++ src/componnents/TableResult.jsx
@@ -1,38 +1,66 @@
-import {useState,useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import "../App.css";
 
 const TableResult = () => {
-      return(
-            <>
-             <section className='m-auto'>
-             <div className='float-left text-emerald-500'>
-                  <h1>INCOMES</h1>
-                  <ul>
-                        <li className='flex justify-between p-2'>
-                              <span>web design</span>
-                              <div className='fle justify-between'>
-                                    <span>50%</span>
-                                    <span>X</span>
-                              </div>
+    return (
+        <section className='flex justify-center items-center'>
+            <div className='flex space-x-10'>
+                <div className='text-emerald-500 w-[550px]'>
+                    <h1 className='text-xl font-bold mb-2'>INCOMES</h1>
+                    <ul className='bg-white rounded-lg'>
+                        <li className='flex justify-between p-2 odd:bg-gray-100 border border-gray-200'>
+                            <span>Web Design</span>
+                            <div className='flex justify-between'>
+                                <span>50%</span>
+                                <span>X</span>
+                            </div>
                         </li>
-                  </ul>
-             </div>
+                        <li className='flex justify-between p-2 odd:bg-gray-100 border border-gray-200'>
+                            <span>Freelancing</span>
+                            <div className='flex justify-between'>
+                                <span>30%</span>
+                                <span>X</span>
+                            </div>
+                        </li>
+                        <li className='flex justify-between p-2 odd:bg-gray-100 border border-gray-200'>
+                            <span>Consulting</span>
+                            <div className='flex justify-between'>
+                                <span>20%</span>
+                                <span>X</span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
 
-             <div className='float-left text-red-600'>
-                  <h1>EXPENSES</h1>
-                  <ul>
-                        <li className='flex justify-between p-2'>
-                              <span>web design</span>
-                              <div className='fle justify-between'>
-                                    <span>50%</span>
-                                    <span>X</span>
-                              </div>
+                <div className='text-red-600  w-[550px]'>
+                    <h1 className='text-xl font-bold mb-2'>EXPENSES</h1>
+                    <ul className='bg-white rounded-lg'>
+                        <li className='flex justify-between p-2 odd:bg-gray-100 border border-gray-200'>
+                            <span>Hosting</span>
+                            <div className='flex justify-between'>
+                                <span>20%</span>
+                                <span>X</span>
+                            </div>
                         </li>
-                  </ul>
-             </div>
-              </section>
-            </>
-      )
+                        <li className='flex justify-between p-2 odd:bg-gray-100 border border-gray-200'>
+                            <span>Software</span>
+                            <div className='flex justify-between'>
+                                <span>10%</span>
+                                <span>X</span>
+                            </div>
+                        </li>
+                        <li className='flex justify-between p-2 odd:bg-gray-100 border border-gray-200'>
+                            <span>Marketing</span>
+                            <div className='flex justify-between'>
+                                <span>15%</span>
+                                <span>X</span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default TableResult;
