a73d118c02ffbac9a76c6d637219592b7a72f401
kiddocoder <ndayikezatresor9@gmail.com>
2024-08-14 13:05:17 +0200

    comm

--- src/componnents/Header.jsx
+++ src/componnents/Header.jsx
@@ -6,11 +6,10 @@ const Header = () =>{
             <>
             <section className="header flex items-center justify-center text-white w-full h-[250px] bg-cover bg-center">
                   <div>
-                        <p className="font-thin text-xl">Available budget in september : </p>
+                        <p className="font-light text-xl">Available budget in september : </p>
                         <p className="text-4xl text-center">+1,12548</p>
                         <div className="flex justify-between p-2 bg-emerald-500">
                              <span className="text-2xl">Incomes</span>
-                             
                         </div>
                   </div>
             </section>
